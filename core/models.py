d7c2e2bfe0bb195c94f7ec1f06e885bd0053e8b8
Cornelius Ezeh <cornzeh@gmail.com>
2024-06-12 20:38:49 -0700

    created the worker model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Worker(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    salary = models.FloatField()
